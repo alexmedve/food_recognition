9784540aad8a132fe8fb8d93f9cdaecc7f74b562
alexmedve <41807747+alexmedve@users.noreply.github.com>
2019-04-24 13:07:53 +0300

    fixed

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,17 +1,39 @@
 <template>
     <v-container>
         <v-layout row wrap>
-            <h1>Test</h1>
+            <h1 @click="getIngredients">Test</h1>
         </v-layout>
     </v-container>
 </template>
 
 <script>
+    import Clarifai from 'clarifai';
+    const clarifaiApp = new Clarifai.App({
+        apiKey: '3a7f9e4ec5474163bec285c80e4dc7ff'
+    });
     export default {
-
+        data() {
+            return {
+                ingredients: [],
+                errors: []
+            }
+        },
+        computed: {
+            ingredients: () => {
+                clarifaiApp.models.predict("bd367be194cf45149e75f01d59f77ba7",
+                    "https://www.blondelish.com/wp-content/uploads/2017/08/1-1.jpg").then(
+                    function (response) {
+                        this.ingredients = response.outputs[0].data.concepts;
+                    },
+                    function (err) {
+                        this.errors.push(err);
+                    }
+                );
+            }
+        }
     }
 </script>
 
-<style>
+<style scoped>
 
 </style>
